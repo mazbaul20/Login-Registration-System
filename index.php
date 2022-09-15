a28db45ee4697c815c03e1c905d7eaf5ff2fbd6e
MD Mazbaul Islam <mazbaul20@gmail.com>
2022-09-15 16:04:03 +0600

    update

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO Dynamic CRUD</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+        <div class="well text-center">
+            <h2>Student List</h2>
+        </div>
+        <div class="panel panel-danger">
+            <div class="panel-heading">
+                <h2>Student List<a class="btn btn-success btn-lg pull-right" href="addstudent.php">Add Student</a></h2>
+            </div>
+            <div class="panel-body">
+                <p>This is main section</p>
+            
+            </div>
+        </div>
+<div class="well">
+            <h3><a href="https://www.youtube.com/channel/UCl1LbPslpUhMPJgoFpbQ96w">Youtube</a>
+                <span class="pull-right"><a href="www.facebook.com/mazbaul2"> Facebook Page</a></span>
+            </h3>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+
+<!-- tutorials 14 start -->
